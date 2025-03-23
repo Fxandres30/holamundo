6b39b9816dd9539e2c204847e66f0c09d30edd2a
Fxandres30 <andres23.t30@gmail.com>
2025-03-23 05:36:30 -0500

    ajustes pequeños

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,13 @@
-const nextConfig = { 
-  output: "standalone", 
+import { NextConfig } from "next";
+
+const nextConfig = {
+  output: "standalone",
+  swcMinify: true,
+  experimental: {
+    swcLoader: true,
+    swcMinify: true,
+    swcWasm: true,
+  },
 };
 
-export default nextConfig;
+export default nextConfig; 
